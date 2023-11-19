ca9d92987341224bcb4e0fa25ae25baeef73a194
Cleber Oliveira <cleber.oliveira@yahoo.com.br>
2023-11-19 18:18:07 -0300

    projeto-concluido

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log("oi")
+let nickname = "Homem de Ferro"
+let nivelXp =9050
+
+if(nivelXp <= 1000){
+    console.log("Ferro")
+}
+else if (nivelXp <= 2000){
+    console.log("Bronze")
+}
+else if(nivelXp <=5000){
+    console.log("Prata")
+}
+else if (nivelXp <=7000){
+    console.log("Ouro")
+}
+else if(nivelXp <=8000){
+    console.log("Platina")
+}
+else if(nivelXp <=9000){
+    console.log("Ascendente")
+}
+else if(nivelXp <=10000){
+    console.log("Imortal")
+}
+else if (nivelXp >=10001){
+    console.log("Radiante")
+}     
+
+console.log("O herói de nome " + nickname + " está no nível de "  + nivelXp)
