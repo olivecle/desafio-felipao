6edc6fda36ad11840f15b171e02df57e34bcc1d2
Cleber Oliveira <cleber.oliveira@yahoo.com.br>
2023-11-21 06:20:02 -0300

    Projeto Melhorado

--- src/index.js
+++ src/index.js
@@ -1,29 +1,29 @@
 let nickname = "Homem de Ferro"
-let nivelXp =9050
+let nivelXp = 9050
+
+console.log("O Herói de nome " + nickname)
 
 if(nivelXp <= 1000){
-    console.log("Ferro")
+    console.log("Está no Xp Ferro")
 }
 else if (nivelXp <= 2000){
-    console.log("Bronze")
+    console.log("Está no Xp Bronze")
 }
-else if(nivelXp <=5000){
-    console.log("Prata")
+else if(nivelXp <= 5000){
+    console.log("Está no Xp Prata")
 }
-else if (nivelXp <=7000){
-    console.log("Ouro")
+else if (nivelXp <= 7000){
+    console.log("Está Xp Ouro")
 }
-else if(nivelXp <=8000){
-    console.log("Platina")
+else if(nivelXp <= 8000){
+    console.log("Está no Xp Platina")
 }
-else if(nivelXp <=9000){
-    console.log("Ascendente")
+else if(nivelXp <= 9000){
+    console.log("Está no Xp Ascendente")
 }
-else if(nivelXp <=10000){
-    console.log("Imortal")
+else if(nivelXp <= 10000){
+    console.log("Está no Xp Imortal")
 }
-else if (nivelXp >=10001){
-    console.log("Radiante")
-}     
-
-console.log("O herói de nome " + nickname + " está no nível de "  + nivelXp)
+else if (nivelXp >= 10001){
+    console.log("Está no Xp Radiante")
+} 
